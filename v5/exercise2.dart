9e92f44609f8f0113d3de6e45ec98581a06c2f8f
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-07-10 09:56:35 +0100

    updating exercise2

--- v5/exercise2.dart
+++ v5/exercise2.dart
@@ -26,15 +26,22 @@ int getTotalSeconds({int hours, int minutes, int seconds}) {
   return totalSeconds;
 }
 
+// Same function, written in a compact way
+int getTotalSecondsCompact({int hours = 0, int minutes = 0, int seconds = 0}) {
+  return (hours*60*60 + minutes * 60 + seconds);
+}
+
+
+
 // to use our function, we need to call it in the main function
 void main() {
   // Our functions return a value, but don't print anything, so we 
   // need to explicitly call print
-  print(getTotalSeconds());
-  print(getTotalSeconds(hours: 1));
-  print(getTotalSeconds(minutes: 1));
-  print(getTotalSeconds(seconds: 1));
-  print(getTotalSeconds(hours: 2, minutes: 2, seconds: 2));
+  print(getTotalSecondsCompact());
+  print(getTotalSecondsCompact(hours: 1));
+  print(getTotalSecondsCompact(minutes: 1));
+  print(getTotalSecondsCompact(seconds: 1));
+  print(getTotalSecondsCompact(hours: 2, minutes: 2, seconds: 2));
 
   // alternatively, we can store the value in a variable and print that
   // or use that variable elsewhere

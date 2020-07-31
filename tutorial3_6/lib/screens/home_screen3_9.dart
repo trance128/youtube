37c3f66406ac8b919993f5314497d73d770f2e44
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-07-31 15:11:54 +0100

    uploading 3_9

---
+++ tutorial3_6/lib/screens/home_screen3_9.dart
@@ -0,0 +1,226 @@
+// Welcome Again to The Complete (FREE) Flutter Course
+// Presented by, Ovidius Mazuru
+
+import 'package:flutter/material.dart';
+
+const PRIMARY = "primary";
+const WHITE = "white";
+const SECONDARY = "secondary";
+
+const Map<String, Color> myColors = {
+  PRIMARY: Color.fromRGBO(226, 115, 138, 1),
+  WHITE: Colors.white,
+  SECONDARY: Color.fromRGBO(247, 225, 227, 1),
+};
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _myAppBar(),
+      backgroundColor: myColors[PRIMARY],
+      body: _Body(),
+    );
+  }
+}
+
+Widget _myAppBar() {
+  return PreferredSize(
+    preferredSize: Size.fromHeight(70),
+    child: AppBar(
+      backgroundColor: myColors[WHITE],
+      elevation: 0,
+      leading: Padding(
+        padding: const EdgeInsets.only(
+          top: 15,
+          left: 15,
+        ),
+        child: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: myColors[PRIMARY],
+            size: 30,
+          ),
+          onPressed: () => print("Menu pressed"),
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(top: 15, right: 15),
+          child: IconButton(
+            onPressed: () => print("Search pressed"),
+            icon: Icon(
+              Icons.search,
+              color: myColors[PRIMARY],
+              size: 30,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+class _Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          flex: 7,
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: myColors[WHITE],
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 30,
+                    bottom: 20,
+                  ),
+                  child: Text(
+                    'Cosmetics that Everyone loves!',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.5,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: LayoutBuilder(builder: (context, constraints) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _ImageTile(
+                          height: constraints.maxHeight * .85,
+                          imagePath: 'assets/images/makeup1.png',
+                          addCallback: () => print("Main item added"),
+                          favoriteCallback: () => print("Main item favorited"),
+                          price: 15.00,
+                        ),
+                        Column(
+                          children: [
+                            _ImageTile(
+                              height: constraints.maxHeight * .37,
+                              imagePath: 'assets/images/makeup2.png',
+                              addCallback: () => print("2nd item added"),
+                              favoriteCallback: () =>
+                                  print("2nd item favorited"),
+                              price: 75.00,
+                            ),
+                            SizedBox(height: 8),
+                            _ImageTile(
+                              height: constraints.maxHeight * .37,
+                              imagePath: 'assets/images/makeup3.png',
+                              addCallback: () => print("3rd item added"),
+                              favoriteCallback: () =>
+                                  print("3rd item favorited"),
+                              price: 55.00,
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  }),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Container(),
+        ),
+      ],
+    );
+  }
+}
+
+class _ImageTile extends StatelessWidget {
+  final double height;
+  final double price;
+  final String imagePath;
+  final Function favoriteCallback;
+  final Function addCallback;
+
+  // highlight this press cntrl + d
+  const _ImageTile({
+    Key key,
+    @required this.height,
+    @required this.price,
+    @required this.imagePath,
+    @required this.favoriteCallback,
+    @required this.addCallback,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              height: height,
+              image: AssetImage(imagePath),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: IconButton(
+                onPressed: () => favoriteCallback,
+                icon: Icon(
+                  Icons.favorite_border,
+                  color: myColors[PRIMARY],
+                  size: 25,
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 5),
+        Row(
+          children: [
+            Text(
+              '\$$price',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(width: 20),
+            InkWell(
+              onTap: () => addCallback,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: myColors[SECONDARY],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(7),
+                    topRight: Radius.circular(7),
+                    bottomLeft: Radius.circular(7),
+                    bottomRight: Radius.circular(15),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Icon(
+                    Icons.add,
+                    color: myColors[PRIMARY],
+                    size: 15,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
